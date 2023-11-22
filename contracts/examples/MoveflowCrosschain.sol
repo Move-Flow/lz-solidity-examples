cf808b873f0ce11e676359d3c41f91d8d91451f6
livelybug <livelybug@gmail.com>
2023-11-22 20:53:41 +0800

    Init

--- contracts/examples/MoveflowCrosschain.sol
+++ contracts/examples/MoveflowCrosschain.sol
@@ -5,15 +5,12 @@ pragma abicoder v2;
 
 import "../lzApp/NonblockingLzApp.sol";
 
-/// @title A LayerZero example sending a cross chain message from a source chain to a destination chain to increment a counter
+/// @title A LayerZero app for cross-chain asset streaming
 contract MoveflowCrosschain is NonblockingLzApp {
     enum PacketType {
         SEND_TO_APTOS,
         RECEIVE_FROM_APTOS
     }
-
-    bytes public constant PAYLOAD = "\x01\x02\x03\x04";
-    uint public counter;
 
     constructor(address _lzEndpoint) NonblockingLzApp(_lzEndpoint) {}
 
@@ -23,25 +20,26 @@ contract MoveflowCrosschain is NonblockingLzApp {
         uint64,
         bytes memory
     ) internal override {
-        counter += 1;
+        // Todo: call withdrawCrossChainRes
     }
 
     function estimateFee(
         uint16 _dstChainId,
         bool _useZro,
-        bytes calldata _adapterParams
+        bytes calldata _adapterParams,
+        uint64 _streamId
     ) public view returns (uint nativeFee, uint zroFee) {
-        return lzEndpoint.estimateFees(_dstChainId, address(this), PAYLOAD, _useZro, _adapterParams);
+        bytes memory payload = abi.encodePacked(uint8(PacketType.SEND_TO_APTOS), _streamId);
+        return lzEndpoint.estimateFees(_dstChainId, address(this), payload, _useZro, _adapterParams);
     }
 
-    function incrementCounter(uint16 _dstChainId, uint64 _streamId) public payable {
+    function withdrawCrossChainReq(uint16 _dstChainId, uint64 _streamId) public payable {
         bytes memory payload = abi.encodePacked(uint8(PacketType.SEND_TO_APTOS), _streamId);
         _lzSend(_dstChainId, payload, payable(msg.sender), address(0x0), bytes(""), msg.value);
     }
 
     function setOracle(uint16 dstChainId, address oracle) external onlyOwner {
         uint TYPE_ORACLE = 6;
-        // set the Oracle
         lzEndpoint.setConfig(lzEndpoint.getSendVersion(address(this)), dstChainId, TYPE_ORACLE, abi.encode(oracle));
     }
 
